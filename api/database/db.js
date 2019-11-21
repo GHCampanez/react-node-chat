be2f99ba0d3a9864c29172e7c55b164e15db7ebf
Guilherme Campanez <guilhermecampanez@gmail.com>
2019-11-21 13:43:03 -0300

    atlas url for mongo

--- api/database/db.js
+++ api/database/db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/chat',{ useNewUrlParser: true });
+const local = 'mongodb://localhost/chat'
+const atlas = 'mongodb+srv://teste:teste@cluster0-bcexw.mongodb.net/test?retryWrites=true&w=majority'
+mongoose.connect(atlas,{ useNewUrlParser: true });
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.Promise = global.Promise;
